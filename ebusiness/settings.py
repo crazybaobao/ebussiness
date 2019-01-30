0cda1914c06abe29aa1c2984d371f81e0f53920e
wangyun <94291333@qq.com>
2019-01-30 16:49:24 +0800

    新增Python环境

--- ebusiness/settings.py
+++ ebusiness/settings.py
@@ -114,7 +114,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_URL = '/upload/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 STATICFILES_FINDERS = (
     "django.contrib.staticfiles.finders.FileSystemFinder",
