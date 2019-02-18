09f1033d9f2971dcdeb80edc431d65d26a4d7f5e
wangyun <94291333@qq.com>
2019-02-18 10:36:29 +0800

    V1.8

--- goods/util.py
+++ goods/util.py
@@ -52,7 +52,8 @@ class Util:
         length = len(request.COOKIES)
         for i in cookie_list:
             if (i == "csrftoken") or (i == "sessionid") or (i.startswith("Hm_lvt_")) or (i.startswith("Hm_lpvt_")) or (
-                    i == "JSESSIONID.07da98ff") or (i == "screenResolution"):
+                    i == "JSESSIONID.07da98ff") or (i == "screenResolution") or (
+                    i == "ACEGI_SECURITY_HASHED_REMEMBER_ME_COOKIE") or (i == "jenkins-timestamper-offset"):
                 length = length - 1
         return length
 
